691f54ba17baabfbf2b5977ba94392fea964c590
Flávio Eler De Melo <flavio.eler@gmail.com>
2024-05-31 00:08:21 +0200

    Update tests

--- tests/test_stability_ai_api.py
+++ tests/test_stability_ai_api.py
@@ -4,18 +4,9 @@
 
 
 import unittest
+# from stability_ai_api import stability_ai_api
 
-from stability_ai_api import stability_ai_api
 
-
-class TestStability_ai_api(unittest.TestCase):
+class TestStabilityAiAPI(unittest.TestCase):
     """Tests for `stability_ai_api` package."""
-
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
+    pass
